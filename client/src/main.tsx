0f38997a2a01b3fa8fb5973d34959c04d64ab983
paul-nguyen96 <paul121596@icloud.com>
2024-12-17 12:08:17 -0600

    mantine installation

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
-import "./styles/output.css"
+import "./styles/output.css";
+import "@mantine/core/styles.css";
 import App from "./App.tsx";
+import { MantineProvider } from "@mantine/core";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </StrictMode>
 );
