8e3cf211ff2a18f5b0ef5e1b26d72706d91d0405
paul-nguyen96 <paul121596@icloud.com>
2024-12-24 05:43:42 -0600

    new navbar

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,11 +1,11 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { Navbar } from "../components/navbar";
+import { TitaniumHeader } from "../components/ui/header";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Navbar />
+      <TitaniumHeader />
       <Outlet />
       <TanStackRouterDevtools />
     </>
