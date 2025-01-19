f4be4a44962f998d2748828513fe375952f6be15
paul-nguyen96 <paul121596@icloud.com>
2025-01-18 21:16:02 -0600

    remove console log

--- client/src/components/ui/chart.tsx
+++ client/src/components/ui/chart.tsx
@@ -100,7 +100,6 @@ export const PieChartMantine = (props: {
       color: getColor(Number(difficulty) - 1),
     })
   );
-  console.log(chartState(pieChartData));
 
   return (
     <Skeleton visible={isLoading}>
