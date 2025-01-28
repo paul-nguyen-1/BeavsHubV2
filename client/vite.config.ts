0c8f10adea4099e52a8e4524293dc35df05575e0
paul-nguyen96 <paul121596@icloud.com>
2025-01-27 18:10:06 -0600

    correct updates

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer()],
-    },
-  },
   optimizeDeps: {
     exclude: ["react-intersection-observer"],
   },
